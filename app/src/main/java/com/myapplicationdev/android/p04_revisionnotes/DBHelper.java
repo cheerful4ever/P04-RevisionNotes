62193544bab10053d594e9e485db7972536dbc35
18016375 <18016375@myrp.edu.sg>
2020-05-14 22:13:34 +0800

    activities 1-10000000

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -23,6 +23,8 @@ public class DBHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		//TODO CREATE TABLE Note
+		String createNoteTableSql = "CREATE TABLE" + TABLE_NOTE + "(" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NOTE_CONTENT + " TEXT, " + COLUMN_STARS + " INTEGER)";
+		 db.execSQL(createNoteTableSql);
 
 	}
 
@@ -34,19 +36,50 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	public void insertNote(String noteContent, int stars) {
 		//TODO insert the data into the database
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+
+		values.put(COLUMN_NOTE_CONTENT, noteContent);
+		values.put(COLUMN_STARS, stars);
+
+		db.insert(TABLE_NOTE, null, values);
+		db.close();
 	}
 
 	public ArrayList<Note> getAllNotes() {
 		//TODO return records in Java objects
+		final ArrayList<Note> notes = new ArrayList<Note>() {
+			String selectQuery = "SELECT " + COLUMN_ID + "," + COLUMN_NOTE_CONTENT + "," + COLUMN_STARS + " FROM " + TABLE_NOTE;
+
+			SQLiteDatabase db = this.getReadableDatabase();
+			Cursor cursor = db.rawQuery(selectQuery, null);
+
+			if(cursor.movetoFirst()) {
+				do{
+					int id = cursor.getInt(0);
+					String title = cursor.getString(1);
+					int Stars = cursor.getInt(2);
+
+					Note note = new Note(id, title, stars);
+					notes.add(note);
+				}
+				while (cursor.moveToNext());
+
+				cursor.close();
+				db.close();
+				return notes;
+			}
+		};
 	}
 
     public ArrayList<String> getNoteContent() {
         //TODO return records in Strings
 
+
 		// Create an ArrayList that holds String objects
         ArrayList<String> notes = new ArrayList<String>();
         // Select all the notes' content
-        String selectQuery = "";
+        String selectQuery = "SELECT note_content from note";
 
         // Get the instance of database to read
         SQLiteDatabase db = this.getReadableDatabase();
@@ -57,7 +90,7 @@ public class DBHelper extends SQLiteOpenHelper {
             // Loop while moveToNext() points to next row and returns true;
             // moveToNext() returns false when no more next row to move to
             do {
-
+            	notes.add(cursor.getString(0));
 
             } while (cursor.moveToNext());
         }
--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -3,11 +3,22 @@ package com.myapplicationdev.android.p04_revisionnotes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        public void onClick(View view) {
+            DBHelper dbHelper = new DBHelper(MainActivity.this);
+
+            dbHelper.insertNote("Revision #1", 4);
+            dbHelper.insertNote("Record #2", 5);
+
+            dbHelper.close();
+        }
     }
 }

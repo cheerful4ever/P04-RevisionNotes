0c95c54f0b12b520907575375c091174dfa3cd2d
18016375 <18016375@myrp.edu.sg>
2020-05-14 22:53:11 +0800

    activities 1-10000000

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/RevisionNotesArrayAdapter.java
@@ -15,6 +15,7 @@ public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
 	ArrayList<Note> notes;
 	int resource;
 	ImageView iv1, iv2, iv3, iv4, iv5;
+	TextView textViewNote;
 
 	public RevisionNotesArrayAdapter(Context context, int resource, ArrayList<Note> notes) {
 		super(context, resource, notes);
@@ -31,16 +32,55 @@ public class RevisionNotesArrayAdapter extends ArrayAdapter<Note> {
 		View rowView = inflater.inflate(resource, parent, false);
 
 		//Match the UI components with Java variables
+		iv1 = rowView.findViewById(R.id.imageView1star);
+		iv2 = rowView.findViewById(R.id.imageView2star);
+		iv3 = rowView.findViewById(R.id.imageView3star);
+		iv4 = rowView.findViewById(R.id.imageView4star);
+		iv5 = rowView.findViewById(R.id.imageView5star);
+		textViewNote = rowView.findViewById(R.id.textViewNote);
 
 		Note note = notes.get(position);
 
-		//Check if the property for starts >= 5, if so, "light" up the stars
-		if (/*stars >= 5*/) {
+		int stars = note.getStars();
+		textViewNote.setText(note.getNoteContent());
+
+		//Check if the property for stars >= 5, if so, "light" up the stars
+		if (stars >= 5) {
 			iv5.setImageResource(android.R.drawable.btn_star_big_on);
 			iv4.setImageResource(android.R.drawable.btn_star_big_on);
 			iv3.setImageResource(android.R.drawable.btn_star_big_on);
 			iv2.setImageResource(android.R.drawable.btn_star_big_on);
 			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 4) {
+			iv5.setImageResource(android.R.drawable.btn_star_big_off);
+			iv4.setImageResource(android.R.drawable.btn_star_big_on);
+			iv3.setImageResource(android.R.drawable.btn_star_big_on);
+			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 3) {
+			iv5.setImageResource(android.R.drawable.btn_star_big_off);
+			iv4.setImageResource(android.R.drawable.btn_star_big_off);
+			iv3.setImageResource(android.R.drawable.btn_star_big_on);
+			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 2) {
+			iv5.setImageResource(android.R.drawable.btn_star_big_off);
+			iv4.setImageResource(android.R.drawable.btn_star_big_off);
+			iv3.setImageResource(android.R.drawable.btn_star_big_off);
+			iv2.setImageResource(android.R.drawable.btn_star_big_on);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else if (stars == 1){
+			iv5.setImageResource(android.R.drawable.btn_star_big_off);
+			iv4.setImageResource(android.R.drawable.btn_star_big_off);
+			iv3.setImageResource(android.R.drawable.btn_star_big_off);
+			iv2.setImageResource(android.R.drawable.btn_star_big_off);
+			iv1.setImageResource(android.R.drawable.btn_star_big_on);
+		} else {
+			iv5.setImageResource(android.R.drawable.btn_star_big_off);
+			iv4.setImageResource(android.R.drawable.btn_star_big_off);
+			iv3.setImageResource(android.R.drawable.btn_star_big_off);
+			iv2.setImageResource(android.R.drawable.btn_star_big_off);
+			iv1.setImageResource(android.R.drawable.btn_star_big_off);
 		}
 
 		return rowView;
